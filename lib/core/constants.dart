ad9a208d5764e7597d4edba246cd0e4ade8722fd
HundefraN <hundefradeveloper@gmail.com>
2026-04-28 15:08:26 +0300

    Clean up: Removing old Flutter files to finalize React migration

--- lib/core/constants.dart
+++
@@ -1,8 +0,0 @@
-class AppConstants {
-  static const String baseUrl = String.fromEnvironment(
-    'BASE_URL',
-    defaultValue: 'https://gdev-backend-kijr.onrender.com',
-  );
-  static const String addisAbabaHeadOfficeId =
-      "ca13d62f-1d8f-4809-9029-b18c03549b45";
-}
