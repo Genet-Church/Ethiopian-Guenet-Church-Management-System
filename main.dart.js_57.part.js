0a4f3f8a85fdfb5a800ff3087506fe8e6b3fa586
HundefraN <hundefradeveloper@gmail.com>
2026-05-10 23:45:33 +0300

    Updates

--- main.dart.js_57.part.js
+++
@@ -1,8 +0,0 @@
-((a,b)=>{a[b]=a[b]||{}})(self,"$__dart_deferred_initializers__")
-$__dart_deferred_initializers__.current=function(a,b,c,$){var B,A,C
-B=c[0]
-A=c[91]
-C=c[97]
-var z=a.updateTypes([]);(function constants(){var y=a.makeConstList
-C.lw=y([A.nE,A.nO],B.v("o<n>"))})()};
-(a=>{a["Z+gNgBOubEgu/MQRKp5Z5+3gxik="]=a.current})($__dart_deferred_initializers__);
